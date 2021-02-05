35c294a2b32edf828c86d26389b3b50bd8f54022
Dodger23 <tarekhassan3874@gmail.com>
2021-02-05 06:13:28 +0200

    V1 working

--- main.py
+++ main.py
@@ -8,9 +8,21 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
+@app.route("/model")
+def mod():
+        return render_template('model.html')
 
+@app.route("/about_us")
+def about():
+        return render_template('about_us.html')
+
+
+@app.route("/predict")
+def predict():
+
+        
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
-if __name__ == '__init__':
+if __name__ == '__main__':
     app.run()
