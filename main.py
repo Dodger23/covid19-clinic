c2fb534d5595cb3322204e4cee17084210f755a1
Dodger23 <tarekhassan3874@gmail.com>
2021-07-13 21:34:57 +0200

    removed everything but model.html

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 from flask import Flask, request, redirect, render_template
-
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def index():
-    return render_template('home.html')
-
-@app.route("/model")
 def mod():
         return render_template('model.html')
 
-@app.route("/about_us")
-def about():
-        return render_template('about_us.html')
 
+url = 'http://3f6dc8ccaebe.ngrok.io/predict'
+@app.route('/predict', methods=['GET', 'POST'])
+@app.route("/model")
+def predict():
+#     files = request.files.getlist('uploadFile')
+    if request.method == 'POST':
+        file = request.files['uploadFile']
+        if file:
+                r = requests.post(url, files={'file':file})
 
-#@app.route("/predict")
-#def predict():
+    return render_template('model.html', message=r.json()['predicted_class'])
 
         
 app.config["TEMPLATES_AUTO_RELOAD"] = True
